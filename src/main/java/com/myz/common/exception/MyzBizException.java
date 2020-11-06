2dffafe9b428b636e98438e4823b2730751eaf08
yzMa <2641007740@qq.com>
2020-11-06 17:01:11 +0800

    ex

---
+++ src/main/java/com/myz/common/exception/MyzBizException.java
@@ -0,0 +1,35 @@
+package com.myz.common.exception;
+
+/**
+ * @author yzMa
+ * @desc
+ * @date 2020/10/13 15:59
+ * @email 2641007740@qq.com
+ */
+public class MyzBizException extends RuntimeException{
+
+    private String code;
+
+    private String msg;
+
+    private Object[] params;
+
+    public MyzBizException(String code, String msg, Object ...params){
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+        this.params = params;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+}
