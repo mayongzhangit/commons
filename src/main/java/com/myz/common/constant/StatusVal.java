5023cae9e0be41311dc66d22ab3dc9e945fe882d
yzMa <2641007740@qq.com>
2020-11-10 10:48:14 +0800

    status

---
+++ src/main/java/com/myz/common/constant/StatusVal.java
@@ -0,0 +1,17 @@
+package com.myz.common.constant;
+
+/**
+ * @author yzMa
+ * @desc
+ * @date 2020/11/9 21:22
+ * @email 2641007740@qq.com
+ */
+public class StatusVal {
+
+    private StatusVal(){}
+
+    public static final Short DELETED = -1;
+
+    public static final Short DEFAULT = 0;
+
+}
